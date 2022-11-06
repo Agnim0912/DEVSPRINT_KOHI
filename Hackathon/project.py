6f5f8fc21459175d13a0fa75563cce5a1868b521
Agnim0912 <agnim0912@gmail.com>
2022-11-06 11:43:55 +0530

    variable add

--- Hackathon/project.py
+++ Hackathon/project.py
@@ -5,7 +5,7 @@ from PyQt5.QtWidgets import QDialog, QApplication, QWidget, QStackedWidget, QCom
 from PyQt5.QtGui import QIcon
 import sqlite3
 
-
+r=6
 
 class InputScreen(QDialog):
     def __init__(self):
